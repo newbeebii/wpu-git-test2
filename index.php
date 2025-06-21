75df69f83e052ff3ab23c951d3e9be5ad8ad4da1
Alfred Sarunan <alfredsarunan01@gmail.com>
2025-06-21 22:26:57 +0800

    menambhakan edit 2

--- index.php
+++ index.php
@@ -8,5 +8,6 @@
 <body>
   <h1>Hellow World</h1>
   <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem accusamus maiores voluptas molestias, excepturi aperiam suscipit earum unde amet expedita, reprehenderit vel dolor cupiditate nam quas architecto debitis autem ducimus.</p>
+  <h2>perubahan 2</h2>
 </body>
 </html>
